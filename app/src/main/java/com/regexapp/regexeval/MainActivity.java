1e2976bb3ec8a8d48c0a9d8f7f6b0d6f7fb6eb1c
Keshav Gupta <keshav1032@gmail.com>
2017-09-05 23:17:45 -0400

    Added regex checker

--- app/src/main/java/com/regexapp/regexeval/MainActivity.java
+++ app/src/main/java/com/regexapp/regexeval/MainActivity.java
@@ -1,11 +1,20 @@
 package com.regexapp.regexeval;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.PatternSyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,10 +45,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button checkButton = (android.widget.Button)findViewById(R.id.checkButton);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText regex = (EditText) findViewById(R.id.regex);
+                String regexString = regex.getText().toString();
+                Log.d("LOG", regexString);
+                EditText toCheck = (EditText) findViewById(R.id.stringcheck);
+                String stringToCheck = toCheck.getText().toString();
+                Log.d("LOG", stringToCheck);
+                TextView result = (TextView) findViewById(R.id.result);
+                try {
+                    if(stringToCheck.matches(regexString)) {
+                        result.setText("Match!");
+                        result.setTextColor(Color.GREEN);
+                    }
+                    else {
+                        result.setText("Match failed.");
+                        result.setTextColor(Color.RED);
+                    }
+                }
+                catch (PatternSyntaxException e) {
+                    Toast.makeText(MainActivity.this,
+                            "Regular Expression not valid. Please check.", Toast.LENGTH_SHORT).show();
+                }
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+            }
+        });
+        //mTextMessage = (TextView) findViewById(R.id.message);
+        //BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
